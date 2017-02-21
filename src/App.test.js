38873c0a6ca44d73533bfa995692e25b9a7ed3f8
jeferbc <jeffe.bernal@gmail.com>
2017-02-20 20:41:41 -0500

    test fix

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from 'enzyme';
 import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
@@ -14,16 +15,38 @@ it('renders one ul tag', () => {
   expect(wrapper.find('ul').length).toBe(1);
 });
 
-it('renders four li tags', () => {
+it('renders three li tags at the beggining', () => {
   const wrapper = render(<App />);
-  expect(wrapper.find('li').length).toBe(4);
+  expect(wrapper.find('li').length).toBe(3);
 });
 
-it('rendered the li with correct text', () => {
+it('rendered the li with correct text at the beggining', () => {
   const wrapper = shallow(<App />);
-  const techs = ['Javascript', 'CSS', 'HTLM', 'React']
-  expect(wrapper.find('li').length).toBe(4);
+  const tasks = ['Sacar la ropa', 'Hacer la cama', 'Leer un rato']
+  expect(wrapper.find('li').length).toBe(3);
   wrapper.find('li').forEach(function(node, i) {
-    expect(node.text()).toBe(techs[i]);
+    expect(node.text()).toBe(tasks[i]);
   });
 });
+
+it('creates a new task with the correct text', () => {
+  const wrapper = mount(<App />);
+  const tasks = ['Sacar la ropa', 'Hacer la cama', 'Leer un rato', 'Hola']
+  const newTask = wrapper.find('#new-task');
+  newTask.node.value = 'Hola';
+  newTask.simulate('change', {target: {value: 'Hola'}})
+  wrapper.find('input').simulate('keyPress', {key: 'Enter'})
+  expect(wrapper.find('li').length).toBe(4);
+  wrapper.find('li').forEach(function(node, i) {
+    expect(node.text()).toBe(tasks[i]);
+  });
+})
+
+it('the text input value is reset after creating task', () => {
+  const wrapper = mount(<App />);
+  const newTask = wrapper.find('#new-task');
+  newTask.node.value = 'Hola';
+  newTask.simulate('change', {target: {value: 'Hola'}})
+  wrapper.find('input').simulate('keyPress', {key: 'Enter'})
+  expect(wrapper.find('#new-task').node.value).toBe('')
+})
